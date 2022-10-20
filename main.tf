c8c55da3e6957d3a4f3137f7fa57ef89bc117c4f
praveenarupi <praveenarupi@gmail.com>
2022-10-20 18:15:36 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -1,31 +1,33 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name =  "${var.env}-vpc"
+    Name = "${var.env}-vpc"
   }
 }
 
 module "subnets" {
   for_each = var.subnets
-  source = "./subnets"
-  name = each.value["name"]
-  subnets = each.value["subnet_cidr"]
-  vpc_id = aws_vpc.main.id
-  AZ = var.AZ
-  env = var.env
+  source   = "./subnets"
+  name     = each.value["name"]
+  subnets  = each.value["subnet_cidr"]
+  vpc_id   = aws_vpc.main.id
+  AZ       = var.AZ
+  env      = var.env
 }
 
 module "routes" {
-  for_each = var.subnets
-  source = "./routes"
-  vpc_id = aws_vpc.main.id
-  name = each.value["name"]
-  subnet_ids = module.subnets
-  gateway_id = aws_internet_gateway.igw.id
-  nat_gateway_id = aws_nat_gateway.ngw.id
-  ngw = try(each.value["ngw"], false)
-  igw = try(each.value["igw"], false)
-  default_vpc_cidr = var.default_vpc_cidr
+  for_each                  = var.subnets
+  source                    = "./routes"
+  vpc_id                    = aws_vpc.main.id
+  name                      = each.value["name"]
+  subnet_ids                = module.subnets
+  gateway_id                = aws_internet_gateway.igw.id
+  nat_gateway_id            = aws_nat_gateway.ngw.id
+  ngw                       = try(each.value["ngw"], false)
+  igw                       = try(each.value["igw"], false)
+  default_vpc_cidr          = var.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering-to-default-vpc.id
 }
 
@@ -54,7 +56,6 @@ resource "aws_vpc_peering_connection" "peering-to-default-vpc" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.default_vpc_id
   auto_accept = true
-
 }
 
 resource "aws_route" "peering-route-on-default-route-table" {
@@ -62,9 +63,3 @@ resource "aws_route" "peering-route-on-default-route-table" {
   destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering-to-default-vpc.id
 }
-
-//### Route53 add
-//resource "aws_route53_zone_association" "zone" {
-//  zone_id = data.aws_route53_zone.private.zone_id
-//  vpc_id  = aws_vpc.main.id
-//}
--- routes/main.tf
+++ routes/main.tf
@@ -4,11 +4,13 @@ resource "aws_route_table" "route-tables" {
     Name = "${var.name}-rt"
   }
 }
+
 resource "aws_route_table_association" "assoc" {
   count          = length(var.subnet_ids[var.name].subnet_ids)
   subnet_id      = element(var.subnet_ids[var.name].subnet_ids, count.index)
   route_table_id = aws_route_table.route-tables.id
 }
+
 
 resource "aws_route" "public" {
   count                  = var.igw ? 1 : 0
@@ -17,11 +19,12 @@ resource "aws_route" "public" {
   gateway_id             = var.gateway_id
 }
 
+
 resource "aws_route" "private" {
   count                  = var.ngw ? 1 : 0
   route_table_id         = aws_route_table.route-tables.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id             = var.nat_gateway_id
+  nat_gateway_id         = var.nat_gateway_id
 }
 
 resource "aws_route" "peering" {
@@ -29,4 +32,3 @@ resource "aws_route" "peering" {
   destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id = var.vpc_peering_connection_id
 }
-

7f3d1cd03db26252254af73c27a29f24b243fb9c
praveenarupi <praveenarupi@gmail.com>
2022-10-20 16:59:23 +0530

    updating ./tf-module-vpc

--- routes/main.tf
+++ routes/main.tf
@@ -5,8 +5,8 @@ resource "aws_route_table" "route-tables" {
   }
 }
 resource "aws_route_table_association" "assoc" {
-  count = length(var.subnet_ids[var.name].out[*].id)
-  subnet_id = element(var.subnet_ids[var.name].out[*].id, count.index)
+  count = length(var.subnet_ids[var.name].out[*].ids)
+  subnet_id = element(var.subnet_ids[var.name].out[*].ids, count.index)
   route_table_id = aws_route_table.route-tables.id
 }
 
